6298bed26f8c76d517653b43e4fe07d0be52c157
associate <associate@pyspark01us.us-central1-c.c.rvc-test.internal>
2025-01-13 20:49:40 +0000

    Initial Commit

---
+++ course/docker-spark-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Remove any existing containers (ignore errors if no containers are found)
+docker rm -f $(docker ps -aq) 2>/dev/null || true
+
+# Run Docker container with corrected format and line continuation
+docker run -it --name spark_container \
+  -p 8888:8888 \
+  -p 4040:4040 \
+  -v "/home/associate/course/code":/home \
+  "spark:latest" /bin/bash
