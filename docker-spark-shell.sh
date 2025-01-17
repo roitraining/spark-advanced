55bbdbcbcaab4af0c7068d6bbbb380df1aaaf3b0
joegagliardo <joey@me.com>
2025-01-17 16:55:59 +0000

    1.0.2

--- docker-spark-shell.sh
+++ docker-spark-shell.sh
@@ -6,5 +6,5 @@ docker rm -f $(docker ps -aq) 2>/dev/null || true
 docker run -it --name spark_container \
   -p 8888:8888 \
   -p 4040:4040 \
-  -v "$HOME/spark-advanced/code":/home \
+  -v "$HOME/class/code":/home \
   "spark:latest" /bin/bash
